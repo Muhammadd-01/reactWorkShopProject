7c10dc6cb2abdbbeaebac944c41c1bf86029ffc5
Muhammad Affan <muhammadaffan1445@gmail.com>
2025-03-10 10:56:14 +0500

    React

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Card from '../components/Card'
+import Footer from '../components/Footer'
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
